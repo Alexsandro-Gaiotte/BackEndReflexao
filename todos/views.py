0fd7aa3e4cd870a94d73e09592bbd3a812b705fe
Alexsandro <alex_gaiotte@hotmail.com>
2024-10-15 20:43:13 -0300

    passo slider 5 concluido

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
 
 
 # Create your views here.
 def home(request):
     return render(request, "todos/home.html")
-
 
 def todo_list_old(request):
     nome = "Rodrigo"
@@ -15,7 +17,14 @@ def todo_list_old(request):
         request, "todos/todo_list.html", {"nome": nome, "lista_alunos": alunos}
     )
 
-
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, "todos/todo_list.html", {"todos": todos})
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+ model = Todo
+ fields = ["title", "deadline"]
+ success_url = reverse_lazy("todo_list")
